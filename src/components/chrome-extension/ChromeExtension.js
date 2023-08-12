ddbdb19a454299836bb6e2da6457b37c25ccf675
effieguenther <effiegguenther@gmail.com>
2023-08-12 15:17:54 +0545

    font and spacing functions

--- src/components/chrome-extension/ChromeExtension.js
+++ src/components/chrome-extension/ChromeExtension.js
@@ -5,19 +5,55 @@ import { useState } from 'react';
 const ChromeExtension = () => {
     const [text, setText] = useState(PARAGRAPHS[0].text);
     const [bionic, setBionic] = useState(false);
+    const [font, setFont] = useState('default');
+    const [lineHeight, setLineHeight] = useState(1.2);
+    const [letterSpacing, setLetterSpacing] = useState(0);
 
     const bionicCheckbox = () => {
         setBionic(!bionic);
     }
 
+    const fontMenu = (e) => {
+        setFont(e.target.value);
+    }
+
+    const increaseLineHeight = () => {
+        const newHeight = parseInt(lineHeight) + 1;
+        setLineHeight(newHeight);
+    }
+
+    const decreaseLineHeight = () => {
+        const newHeight = parseInt(lineHeight) - 1;
+        setLineHeight(newHeight);
+    }
+
+    const increaseLetterSpacing = () => {
+        const newSpacing = parseInt(letterSpacing) + 1;
+        setLetterSpacing(newSpacing);
+    }
+
+    const decreaseLetterSpacing = () => {
+        const newSpacing = parseInt(letterSpacing) - 1;
+        setLetterSpacing(newSpacing);
+    }
+
+    const styles = {
+        fontFamily: font,
+        lineHeight: lineHeight,
+        letterSpacing: letterSpacing
+    }
+
+    //conditional rendering of text from the stateful variable 'bionic'
     const BionicRender = () => {
         const words = text.split(" ");
         return (bionic === false) ?
             ( 
-                <p>{text}</p>
+                <div style={styles}>
+                    <p>{text}</p>
+                </div>
             ) :
             (
-                <>
+                <div style={styles} className='bionic'>
                     {
                         words.map((word) => {
                             const halfLength = Math.ceil(word.length / 2);
@@ -25,12 +61,13 @@ const ChromeExtension = () => {
                             const secondHalf = word.slice(halfLength);
                             return (
                                 <>
-                                    <p className='bionic'><span className='bionicBold'>{firstHalf}</span>{secondHalf} </p>
+                                    <p className='bionic'>
+                                        <span className='bionicBold'>{firstHalf}</span>{secondHalf} </p>
                                 </>
                             )
                         })
                     }
-                </>
+                </div>
             )
     }
 
@@ -49,18 +86,18 @@ const ChromeExtension = () => {
                         <span class="slider"></span>
                     </label>
                     <p>Change Font</p>
-                    <select name="fonts" id="fonts">
+                    <select name="fonts" id="fonts" onChange={fontMenu}>
                         <option value="">Default</option>
                         <option value="arial">Arial</option>
                         <option value="helvetica">Helvetica</option>
                         <option value="Comic Sans MS">Comic Sans MS</option>
                     </select>
                     <p>Letter Spacing</p>
-                    <button id="letterSmaller">-</button>
-                    <button id="letterBigger">+</button>
+                    <button onClick={decreaseLetterSpacing}>-</button>
+                    <button onClick={increaseLetterSpacing}>+</button>
                     <p>Line Spacing</p>
-                    <button id="lineSmaller">-</button>
-                    <button id="lineBigger">+</button>
+                    <button onClick={decreaseLineHeight}>-</button>
+                    <button onClick={increaseLineHeight}>+</button>
 
                 </Card>
             </Col>
