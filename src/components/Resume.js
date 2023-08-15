c1b17b80f526aad58e0cfc5be9eedff1b3d36830
effieguenther <effiegguenther@gmail.com>
2023-08-15 22:32:53 +0545

    resume styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,48 +3,57 @@ import { Col, Row } from 'reactstrap';
 const Resume = () => {
     return (
         <>
-            <Row className='project-header'>
-                <h6>Skills</h6>
+            <Row className='ms-1 mt-4'>
+                <h5>Skills</h5>
+            </Row>
+            <Row className='my-4 ms-1'>
+                <h6>Technical Skills</h6>
             </Row>
             <Row>
-                <ul className='skills-list'>
-                    <li>HTML/CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>React Native</li>
-                    <li>Redux</li>
-                    <li>Firebase</li>
-                    <li>Bootstrap</li>
-                    <li>NodeJS</li>
-                    <li>MongoDB</li>
-                    <li>Github</li>
-                    <li>Python</li>
-                    <li>Microsoft Office</li>
-                    <li>Adobe Photoshop/Illustrator</li>
-                </ul>
-                <ul>
-                    <li>Communication</li>
-                    <li>Calm under pressure</li>
-                    <li>Attention to detail</li>
-                    <li>Emotional intelligene</li>
-                </ul>
+                <div className='skills-list'>
+                    <p>HTML/CSS</p>
+                    <p>JavaScript</p>
+                    <p>React</p>
+                    <p>React Native</p>
+                    <p>Redux</p>
+                    <p>Firebase</p>
+                    <p>Bootstrap</p>
+                    <p>NodeJS</p>
+                    <p>MongoDB</p>
+                    <p>Github</p>
+                    <p>Python</p>
+                    <p>Microsoft Office</p>
+                    <p>Photoshop</p>
+                    <p>Illustrator</p>
+                </div>
             </Row>
-            <Row className='project-header'>
-                <h6>Education</h6>
+            <Row className='ms-1 my-4'>
+                <h6>Soft Skills</h6>
             </Row>
             <Row>
-                <p>Nucamp Full Stack Program - 2023</p>
-                <p>Skills accquired: React, React Native, Redux, Firebase, Bootstrap, NodeJS, MongoDB</p>
-                <p>Boot.dev Backend Program - 2022</p>
-                <p>Skills accquired: Python, JavaScript, OOP, Algorithms, Data Structures, HTTP</p>
-                <p>The University of Alabama - 2018</p>
-                <p>B.S. with a Minor in Computer Science</p>
+                <div className='skills-list'>
+                    <p>Communication</p>
+                    <p>Calm under pressure</p>
+                    <p>Attention to detail</p>
+                    <p>Emotional intelpgene</p>
+                </div>    
             </Row>
-            <Row className='project-header'>
-                <h6>My Story</h6>
+            <Row className='ms-1 my-4'>
+                <h5>Education</h5>
             </Row>
             <Row>
-                <p>After 4 years as a product developer for luxury fashion companies in New York City, I realized that I wanted a different career. I have always had an interest in coding since being on the robotics team in high school, and continued my study through a minor in college. I rekindled my love for software with the Boot.dev program, and really committed to this path after joning Nucamp's full stack course. Over the past year I have worked on my passion projects and started freelancing with Upwork.</p>
+                <p className='school-header'>Nucamp Full Stack Program - 2023</p>
+                <p className='school-desc'>Skills accquired: React, React Native, Redux, Firebase, Bootstrap, NodeJS, MongoDB</p>
+                <p className='school-header'>Boot.dev Backend Program - 2022</p>
+                <p className='school-desc'>Skills accquired: Python, JavaScript, OOP, Algorithms, Data Structures, HTTP</p>
+                <p className='school-header'>The University of Alabama - 2018</p>
+                <p className='school-desc'>B.S. with a Minor in Computer Science</p>
+            </Row>
+            <Row className='ms-1'>
+                <h5>My Story</h5>
+            </Row>
+            <Row>
+                <p className='story-desc'>After 4 years as a product developer for luxury fashion companies in New York City, I realized that I wanted a different career. I have always had an interest in coding since being on the robotics team in high school, and continued my study through a minor in college. I rekindled my love for software with the Boot.dev program, and really committed to this path after joning Nucamp's full stack course. Over the past year I have worked on my passion projects and started freelancing with Upwork.</p>
             </Row>
         </>
     )
